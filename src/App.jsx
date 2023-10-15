fa3b6c6a708140c46b08de873039fccce0921708
Alme <josealejandrogilmendez08@gmail.com>
2023-10-15 14:25:41 -0400

    Set the select difficulty screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,21 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import Game from "./Game";
 
-function App() {
-  const [data, setData] = useState([]);
-  const [level, setLevel] = useState(1);
-  const [gameState, setGameState] = useState('loading');
+function App(){
+  const [level, setLevel] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const promises = [];
-        const random = [];
-
-        while (random.length <  (level * 5)) {
-          const id = Math.floor(Math.random() * 151) + 1
-          if(!random.includes(id))random.push(id);
-        }
-        for (let i = 0; i < random.length; i++) promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${random[i]}`));
-
-        const responses = await Promise.all(promises);
-        const jsonResponses = await Promise.all(responses.map(response => response.json()));
-        setData(jsonResponses)
-        setGameState('running')
-      } catch (error) {
-        setGameState('error')
-      }
-    };
-
-    setGameState('loading')
-    fetchData()
-  }, [level])
-
-  if(gameState === 'loading'){
+  if (level !== null) {
     return(
-      <h1>Loading...</h1>
+      <Game level={level}/>
     )
   }
-  if(gameState === 'error'){
-    return(
-      <h1>Error</h1>
-    )
-  }
-  if(gameState === 'gameOver'){
-    return(
-      <h1>Game Over</h1>
-    )
-  }
-  return (
+
+  return(
     <>
-      hello world
+      <button onClick={() => setLevel(1)}>Easy</button>
+      <button onClick={() => setLevel(2)}>Medium</button>
+      <button onClick={() => setLevel(3)}>Hard</button>
     </>
   )
 }
-
 export default App
