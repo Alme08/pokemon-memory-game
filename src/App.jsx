400b2a6f893bd5f2120dd4dde94bda527d7b6ef4
Alme <josealejandrogilmendez08@gmail.com>
2023-10-15 19:47:21 -0400

    update return and import MusicPlayer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Game from "./Game";
+import MusicPlayer from "./components/MusicPlayer";
 
 function App(){
   const [level, setLevel] = useState(null);
@@ -12,9 +13,18 @@ function App(){
 
   return(
     <>
-      <button onClick={() => setLevel(1)}>Easy</button>
-      <button onClick={() => setLevel(2)}>Medium</button>
-      <button onClick={() => setLevel(3)}>Hard</button>
+      <div className="startScreen">
+        <div className="image-container">
+          <img className="logo" src='src\assets\logo.png' alt="pokemon logo" />
+        </div>
+        <h1>Memory Game</h1>
+        <div className="groupButton">
+          <button onClick={() => setLevel(1)}>Easy</button>
+          <button onClick={() => setLevel(2)}>Medium</button>
+          <button onClick={() => setLevel(3)}>Hard</button>
+        </div>
+      </div>
+      <MusicPlayer/>
     </>
   )
 }
