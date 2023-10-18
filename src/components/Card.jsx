db5490c34aa46cf4720a5f75e3703e6ce9c7286b
Alme <josealejandrogilmendez08@gmail.com>
2023-10-17 21:25:00 -0400

    Import tilt component and return it

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,25 @@
+// import { useState } from "react";
+import Tilt from "react-parallax-tilt";
+
 function Card({name, img, id, handleCardClick, isFlipped}) {
+
     return(
-        <div className={`card ${isFlipped ? 'flipped' : ''}`} data-id={id}>
-                <div className="front" onClick={() => handleCardClick(id)} >
+        <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={() => handleCardClick(id)} >
+            <Tilt   tiltReverse
+                    reset
+                    glareEnable={true}
+                    glareMaxOpacity={0.4}
+                    glareColor="#ffffff"
+                    glarePosition="all"
+                    glareBorderRadius="20px"
+                    className="tilt">
+
+                <div className="front" >
                     <img src={img} />
                     <p>{name}</p>
                 </div>
-
-                <div className="back">Back</div>
+                <div className="back"></div>
+            </Tilt>
         </div>
     )
 }
