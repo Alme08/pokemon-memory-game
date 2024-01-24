571db7e580ffaea062ba195601428f59892e3530
Alme <josealejandrogilmendez08@gmail.com>
2024-01-24 18:31:50 -0400

    Create gameOver component

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,38 @@
+import { motion } from "framer-motion"
+
+function GameOver({result, restartTheGame, quit}) {
+    return(
+        <>
+            <motion.div 
+                className={result === 'win' ? 'win' : 'lose'}
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                transition={{duration: 0.7}}
+                exit={{opacity: 0}}
+            >
+                <div>
+                    {result === 'win' ? 'You win!' : 'You lose!'}
+                </div>
+                <button onClick={ () => {
+                    restartTheGame()
+                }}>
+                        Restart
+                </button>
+                <button onClick={ () => {
+                    quit()
+                }}>
+                        Quit
+                </button>
+            </motion.div>
+            <motion.div 
+                initial={{opacity: 0}}
+                animate={{opacity: 0.6}}
+                transition={{duration: 0.7}}
+                exit={{opacity: 0}}
+                className='overlay'>
+            </motion.div>
+        </>
+    )
+}
+
+export default GameOver
