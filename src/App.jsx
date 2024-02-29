b29e75d1a3951d29387d26811b5a596b7c7d028a
Alme <josealejandrogilmendez08@gmail.com>
2024-02-29 18:09:04 -0400

    import logo like a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import Game from './Game';
 import MusicPlayer from './components/MusicPlayer';
+import pokemonLogo from './assets/logo.png';
 
-function App(){
-  const [level, setLevel] = useState(null);
-  const [bestScore, setBestScore] = useState(0);
+function App() {
+	const [level, setLevel] = useState(null);
+	const [bestScore, setBestScore] = useState(0);
 
-  return(
-    <>{ level === null ? 
-      <div className='startScreen'>
-        <div className='image-container'>
-          <img className='logo' src='src\assets\logo.png' alt='pokemon logo' />
-        </div>
-        <h1>Memory Game</h1>
-        <div className='groupButton'>
-          <button onClick={() => setLevel(1)}>Easy</button>
-          <button onClick={() => setLevel(2)}>Medium</button>
-          <button onClick={() => setLevel(3)}>Hard</button>
-        </div>
-      </div> :
-      <Game level={level} setLevel={setLevel} bestScore={bestScore} setBestScore={setBestScore}/>}
-      <MusicPlayer/>
-    </>
-  )
+	return (
+		<>
+			{level === null ? (
+				<div className='startScreen'>
+					<div className='image-container'>
+						<img className='logo' src={pokemonLogo} alt='pokemon logo' />
+					</div>
+					<h1>Memory Game</h1>
+					<div className='groupButton'>
+						<button onClick={() => setLevel(1)}>Easy</button>
+						<button onClick={() => setLevel(2)}>Medium</button>
+						<button onClick={() => setLevel(3)}>Hard</button>
+					</div>
+				</div>
+			) : (
+				<Game
+					level={level}
+					setLevel={setLevel}
+					bestScore={bestScore}
+					setBestScore={setBestScore}
+				/>
+			)}
+			<MusicPlayer />
+		</>
+	);
 }
-export default App
+export default App;
