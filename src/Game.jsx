5e234f16fac356e794f98999bf5636f4f411a3e1
Alme <josealejandrogilmendez08@gmail.com>
2024-02-29 18:14:48 -0400

    import assets as components

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,154 +1,162 @@
-import { useState, useEffect } from "react"
-import { AnimatePresence } from "framer-motion";
-import Card from "./components/Card";
-import GameOver from "./components/GameOver";
+import { useState, useEffect } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import Card from './components/Card';
+import GameOver from './components/GameOver';
+import imageLoader from './assets/pokeball-min.png';
+import logo from './assets/logo.png';
 
 function Game({ level, setLevel, bestScore, setBestScore }) {
-  const [data, setData] = useState([]);
-  const [gameStatus, setGameStatus] = useState('loading');
-  const [score, setScore] = useState(0);
-  const [clickedCards, setClickedCards] = useState([]);
-  const [isFlipped, setIsFlipped] = useState(true);
-  const [isClicked, setIsClicked] = useState(false);
+	const [data, setData] = useState([]);
+	const [gameStatus, setGameStatus] = useState('loading');
+	const [score, setScore] = useState(0);
+	const [clickedCards, setClickedCards] = useState([]);
+	const [isFlipped, setIsFlipped] = useState(true);
+	const [isClicked, setIsClicked] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const promises = [];
-        const random = [];
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const promises = [];
+				const random = [];
 
-        while (random.length <  (level === 3 ? 14 : (level * 5))) {
-          const id = Math.floor(Math.random() * 151) + 1
-          if(!random.includes(id))random.push(id);
-        }
-        for (let i = 0; i < random.length; i++) promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${random[i]}`));
+				while (random.length < (level === 3 ? 14 : level * 5)) {
+					const id = Math.floor(Math.random() * 151) + 1;
+					if (!random.includes(id)) random.push(id);
+				}
+				for (let i = 0; i < random.length; i++)
+					promises.push(
+						fetch(`https://pokeapi.co/api/v2/pokemon/${random[i]}`)
+					);
 
-        setTimeout(async () => {
-            const responses = await Promise.all(promises);
-            const jsonResponses = await Promise.all(responses.map((response) => response.json()));
-            setData(jsonResponses);
-            setGameStatus('running');
-          }, 3000);
-      } catch (error) {
-        setGameStatus('error')
-      }
-    };
+				setTimeout(async () => {
+					const responses = await Promise.all(promises);
+					const jsonResponses = await Promise.all(
+						responses.map(response => response.json())
+					);
+					setData(jsonResponses);
+					setGameStatus('running');
+				}, 3000);
+			} catch (error) {
+				setGameStatus('error');
+			}
+		};
 
-    setGameStatus('loading')
-    fetchData()
-  }, [level])
+		setGameStatus('loading');
+		fetchData();
+	}, [level]);
 
-  useEffect(() => {
+	useEffect(() => {
+		// If shuffle happens after 1,3s then some cards appear without
+		//flipping affect
+		const shuffleTimer = setTimeout(() => {
+			randomArray();
+		}, 800);
+		const flipTimer = setTimeout(() => {
+			// Reset flipped cards after a delay
+			setIsFlipped(!isFlipped);
+			setIsClicked(false);
+		}, 1300);
 
-    // If shuffle happens after 1,3s then some cards appear without
-    //flipping affect 
-    const shuffleTimer = setTimeout(() => {
-      randomArray();
-    }, 800)
-    const flipTimer = setTimeout(() => {
+		return () => {
+			clearTimeout(flipTimer);
+			clearTimeout(shuffleTimer);
+		};
+	}, [clickedCards]); // eslint-disable-line react-hooks/exhaustive-deps
 
-      // Reset flipped cards after a delay
-      setIsFlipped(!isFlipped);
-      setIsClicked(false)
-    }, 1300);
+	const randomArray = () => {
+		const randomData = [...data];
+		for (let i = randomData.length - 1; i > 0; i -= 1) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[randomData[i], randomData[j]] = [randomData[j], randomData[i]];
+		}
+		setData(randomData);
+	};
 
-    return () => {
-      clearTimeout(flipTimer);
-      clearTimeout(shuffleTimer);
-    }
-  }, [clickedCards]);// eslint-disable-line react-hooks/exhaustive-deps
+	const handleCardClick = cardId => {
+		// Prevents user from multiple clicks while card is flipping
+		//after timeout below isClicked is false again and user can click
+		//on the card
+		setIsClicked(true);
+		if (isClicked) return;
 
-  const randomArray = () => {
-    const randomData = [...data];
-    for (let i = randomData.length - 1; i > 0; i -= 1) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [randomData[i], randomData[j]] = [randomData[j], randomData[i]];
-    }
-    setData(randomData);
-  };
+		// If the card has already been clicked, reset the current score and the clicked card array
+		if (clickedCards.includes(cardId)) {
+			setGameStatus('gameOver');
+		} else {
+			setIsFlipped(!isFlipped);
+			const newScore = score + 1;
+			setScore(newScore);
+			if (newScore > bestScore) setBestScore(newScore);
+			setClickedCards([...clickedCards, cardId]);
+			if (clickedCards.length === data.length - 1) setGameStatus('win');
+		}
+	};
 
-  const handleCardClick = (cardId) => {
+	const restartTheGame = () => {
+		setIsFlipped(!isFlipped);
+		setScore(0);
+		setClickedCards([]);
+		setGameStatus('running');
+	};
 
-    // Prevents user from multiple clicks while card is flipping
-    //after timeout below isClicked is false again and user can click
-    //on the card
-    setIsClicked(true)
-    if(isClicked) return
+	const quit = () => {
+		setLevel(null);
+	};
 
-    // If the card has already been clicked, reset the current score and the clicked card array
-    if (clickedCards.includes(cardId)) {
-      setGameStatus('gameOver');
-    } 
-    else {
-      setIsFlipped(!isFlipped)
-      const newScore = score + 1;
-      setScore(newScore);
-      if (newScore > bestScore) setBestScore(newScore);
-      setClickedCards([...clickedCards, cardId]);
-      if (clickedCards.length === data.length - 1) setGameStatus('win');
-    }
-  };
+	if (gameStatus === 'loading') {
+		return (
+			<div className='loader'>
+				<img src={imageLoader} alt='Imagen de carga' />
+				<p>Loading...</p>
+			</div>
+		);
+	}
 
-  const restartTheGame = () => {
-    setIsFlipped(!isFlipped)
-    setScore(0)
-    setClickedCards([])
-    setGameStatus('running')
-  }
+	if (gameStatus === 'error') {
+		return (
+			<div className='error'>
+				<h1>Something bad happened :c</h1>
+				<button onClick={quit}>Retry</button>
+			</div>
+		);
+	}
 
-  const quit = () => {
-    setLevel(null)
-  }
+	return (
+		<div className='game'>
+			<div className='info'>
+				<img src={logo} alt='logo' onClick={quit} />
+				<div>
+					<p>Score: {score}</p>
+					<p>High Score: {bestScore}</p>
+				</div>
+			</div>
 
-  if(gameStatus === 'loading'){
-    return(
-        <div className='loader'>
-            <img src='src\assets\pokeball-min.png' alt='Imagen de carga'/>
-            <p>Loading...</p>
-        </div>
-    )
-  }
+			<div className='cards'>
+				{data.map(pokemon => {
+					return (
+						<Card
+							name={pokemon.name}
+							img={pokemon.sprites.front_default}
+							id={pokemon.id}
+							handleCardClick={handleCardClick}
+							isFlipped={isFlipped}
+							key={pokemon.id}
+						/>
+					);
+				})}
+			</div>
 
-  if(gameStatus === 'error'){
-    return(
-      <div className='error'>
-        <h1>Something bad happened :c</h1>
-        <button onClick={quit}>Retry</button>
-      </div>
-    )
-  }
-
-  return (
-    <div className='game'>
-        <div className='info'>
-            <img src='src\assets\logo.png' alt='logo' onClick={quit} />
-            <div>
-                <p>Score: {score}</p>
-                <p>High Score: {bestScore}</p>
-            </div>
-        </div>
-    
-        <div className='cards'>
-          {data.map((pokemon) => {
-              return (
-                  <Card 
-                    name={pokemon.name} 
-                    img={pokemon.sprites.front_default} 
-                    id={pokemon.id} 
-                    handleCardClick={handleCardClick} 
-                    isFlipped={isFlipped}
-                    key={pokemon.id}/>
-              )
-          })}
-        </div>
-
-        <AnimatePresence>
-          {(gameStatus === 'gameOver' || gameStatus === 'win') && 
-            <GameOver result={gameStatus} restartTheGame={restartTheGame} quit={quit}/>
-          }
-        </AnimatePresence>
-    </div>
-  )
+			<AnimatePresence>
+				{(gameStatus === 'gameOver' || gameStatus === 'win') && (
+					<GameOver
+						result={gameStatus}
+						restartTheGame={restartTheGame}
+						quit={quit}
+					/>
+				)}
+			</AnimatePresence>
+		</div>
+	);
 }
 
 export default Game;
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,32 +1,36 @@
 import { useRef, useState } from 'react';
+import ost from '../assets/ost.mp3';
 
 function MusicPlayer() {
-  const audioRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
+	const audioRef = useRef(null);
+	const [isPlaying, setIsPlaying] = useState(false);
 
+	const togglePlay = () => {
+		if (audioRef.current) {
+			if (isPlaying) {
+				audioRef.current.pause();
+			} else {
+				audioRef.current.play();
+			}
+			setIsPlaying(!isPlaying);
+		}
+	};
 
-  const togglePlay = () => {
-    if (audioRef.current) {
-      if (isPlaying) {
-        audioRef.current.pause();
-      } else {
-        audioRef.current.play();
-      }
-      setIsPlaying(!isPlaying);
-    }
-  };
-
-  return (
-    <div className='musicPlayer'>
-      <button onClick={togglePlay}>
-        {isPlaying ? <i className='fa-solid fa-volume-high'></i> : <i className='fa-solid fa-volume-xmark'></i>}
-      </button>
-      <audio ref={audioRef} loop>
-        <source src='src\assets\ost.mp3' type='audio/mpeg' />
-        Tu navegador no soporta la reproducción de audio.
-      </audio>
-    </div>
-  );
+	return (
+		<div className='musicPlayer'>
+			<button onClick={togglePlay}>
+				{isPlaying ? (
+					<i className='fa-solid fa-volume-high'></i>
+				) : (
+					<i className='fa-solid fa-volume-xmark'></i>
+				)}
+			</button>
+			<audio ref={audioRef} loop>
+				<source src={ost} type='audio/mpeg' />
+				Tu navegador no soporta la reproducción de audio.
+			</audio>
+		</div>
+	);
 }
 
 export default MusicPlayer;
